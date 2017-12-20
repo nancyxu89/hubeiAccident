bf74ac39c9fbf9a4031b569e10074915571679eb
徐媛媛 <837756906@qq.com>
2017-12-20 16:37:46 +0800

    Add files via upload

---
+++ dist/static/js/config.js
@@ -0,0 +1,5 @@
+(function () {
+  // window.baseUrl = 'http://mock.eolinker.com/hD84b2B3a587494f830c97bd730d97dd45b23191d1c6c6c?uri='
+  window.baseUrl = 'http://192.168.200.11:8081'
+  // window.baseUrl = 'http://192.168.20.223:8081'
+}())
